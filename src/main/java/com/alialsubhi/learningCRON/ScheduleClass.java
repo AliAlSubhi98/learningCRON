d9c8e1a4bf890c41867ac30fc72c806dd5879328
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 15:45:11 +0400

    Problem 47: Write a CRON expression that runs a job at 1:30 PM on the second to last Friday of every month.

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 0 22-23/2,0-5/2 * * *")
+    @Scheduled(cron = "0 30 13 8-31 * 5")
     public void schedule(){
         System.out.println("""
                 Scheduled ! *_* 
