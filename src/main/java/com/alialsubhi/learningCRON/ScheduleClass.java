945ba474664f945495207db8598336d46009f629
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 14:13:39 +0400

    Write a CRON expression that runs a job every 30 minutes during business hours (8:00 AM to 5:00 PM) on weekdays

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,11 +9,10 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 0 * * 1 *")
+    @Scheduled(cron = "0 */30 8-17 * * 0-4")
     public void schedule(){
         System.out.println("""
                 Write a CRON expression that:
-                that runs a job every hour during the month of January.
-                 """);
+                that runs a job every 30 minutes during business hours (8:00 AM to 5:00 PM) on weekdays                 """);
     }
 }
