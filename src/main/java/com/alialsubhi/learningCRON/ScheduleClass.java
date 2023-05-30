5437614b41c75b65dc2dba85f717bbb0ce05a3ab
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 14:30:55 +0400

    Problem 19: Write a CRON expression that runs a job every hour but only on even-numbered days of the month

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 30 23 * * 5")
+    @Scheduled(cron = "0 0 * */2 * *")
     public void schedule(){
         System.out.println("""
                 Scheduled ! *_* 
