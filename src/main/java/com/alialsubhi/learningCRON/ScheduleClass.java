74c7154b665f9a01bc37729927aded7f9bb17ab8
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 14:26:52 +0400

    Problem 14: Write a CRON expression that runs a job every 5 minutes on weekdays and every 15 minutes on weekends

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,12 +9,11 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 0 18 * * 0-6")
+    @Scheduled(cron = "0 */5 * * * 0-4")
+    @Scheduled(cron = "0 */15 * * * 5-6")
     public void schedule(){
         System.out.println("""
-                Write a CRON expression that:
-                 that runs a job every day at 6:00 PM during daylight saving time
-                (DST) in the United States.
+                Scheduled ! *_* 
                 """);
     }
 }
