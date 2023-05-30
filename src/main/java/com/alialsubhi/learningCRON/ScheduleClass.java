db5dea3e840fce1f475635843f951b8088fb04f3
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 14:53:49 +0400

    Problem 43: Write a CRON expression that runs a job at 4:30 PM on weekdays in July and August.

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 */5 * 1-10 1 *")
+    @Scheduled(cron = "0 30 16 * 7,8 0-4")
     public void schedule(){
         System.out.println("""
                 Scheduled ! *_* 
