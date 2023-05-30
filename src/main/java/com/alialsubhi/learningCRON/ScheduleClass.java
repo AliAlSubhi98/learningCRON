b58210530668f03b962efbdae6ef9be513047adc
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 14:28:51 +0400

    Problem 16: Write a CRON expression that runs a job every hour during the first half of the month

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 */10 9-18 * * 0-4")
+    @Scheduled(cron = "0 0 * 1-15 * * ")
     public void schedule(){
         System.out.println("""
                 Scheduled ! *_* 
