bf15b75440b0832eb59ed1ef4c378cfe2fcb73fd
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 14:35:49 +0400

    Problem 21: Write a CRON expression that runs a job at 3:30 AM every day except Saturday and Sunday.

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 */5 9-18 * * 5-6")
+    @Scheduled(cron = "0 30 3 * * 1-5")
     public void schedule(){
         System.out.println("""
                 Scheduled ! *_* 
