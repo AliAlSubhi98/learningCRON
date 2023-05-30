37c7d1c668159a2a92f8e9a7eeae4514134167fc
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 14:11:58 +0400

    Write a CRON expression that runs a job every hour during the month of January

--- src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
+++ src/main/java/com/alialsubhi/learningCRON/ScheduleClass.java
@@ -9,11 +9,11 @@ import java.util.Date;
 public class ScheduleClass {
 /*Second      Minute        Hour       Day      Month       Day of Week
   0-59        0-59          0-23       1-31     1-12        0-6  {0-> Sunday}*/
-    @Scheduled(cron = "0 0 3 * * *")
+    @Scheduled(cron = "0 0 * * 1 *")
     public void schedule(){
         System.out.println("""
                 Write a CRON expression that:
-                that runs a job every day at 3:00 AM.
+                that runs a job every hour during the month of January.
                  """);
     }
 }
